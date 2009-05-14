f92f30592446aa13d15a3cffcdd00392ac36c5d2
yaanno <yaanno@gmail.com>
2009-05-14 17:50:25 +0200

    appearance and functional updates

--- kommentkiller.user.js
+++ kommentkiller.user.js
@@ -24,22 +24,105 @@
 // ==/UserScript==
 
 // TODO externalize the identitifers
-var knownCommentIdentifiers = ['comment','commentlist', 'comments', 'respond', 'cnnComments','comment_container','hozzaszolas'],
-commentBlocks = [];
+
+var kk_knownBlocks = "comment commentlist comments respond cnnComments comment_container hozzaszolas";
+var kk_currentBlocks = [];
+var kk_contentWrapper = document.createElement("div");
 
 window.pageLoaded = function(){
-  kommentKillerPanel = document.createElement("div");
-  kommentKillerPanel.innerHTML = '<div id="kommentkillerpanel"><style> #kommentkillerpanel {display:none;width:200px;height:80px;background:#000;color:#fff; position:absolute;z-index:1000}</style><p>This will be the panel</p></div>';
-  document.body.insertBefore(kommentKillerPanel, document.body.firstChild);
+  var kk_content = '<div id="kk_panel">';
+  kk_content += '<style>';
+  kk_content += '#kk_panel {display:none;width:200px;padding:15px;background:#333;color:#fff; position:fixed;top:0;left:20px;z-index:1000;font-size:11px; font-family:Helvetica;border: 5px solid #666;border-top:none}';
+  kk_content += '#kk_panel *{margin:0;padding:0} #kk_panel p{padding: 5px; line-height: 16px}';
+  kk_content += '#kk_panel a:link, #kk_panel a:visited{color:#fff} #kk_panel a:hover{color:#ddd}';
+  kk_content += '</style>';
+  kk_content += '<p><input id="kk_harvest_option" type="checkbox"> <label for="kk_harvest_option">Harvest all comments by default</label></p>';
+  kk_content += '<p><input id="kk_hide_option" type="checkbox"> <label for="kk_harvest_option">Hide all comments by default</label></p>';
+  kk_content += '<p><input id="kk_hidecomments" type="button" value="Hide all"> or ';
+  kk_content += '<input id="kk_showcomments" type="button" value="Show all"></p>';
+  kk_content += '</div>';
+  
+  kk_contentWrapper.innerHTML = kk_content;
+  document.body.insertBefore(kk_contentWrapper, document.body.firstChild);
+  
+  if(!GM_getValue("kk_blockCache")){
+    GM_setValue("kk_blockCache",kk_knownBlocks);
+  }
+  
+  if(!GM_getValue("kk_harvestCommentsDefault")){
+    GM_setValue("kk_harvestCommentsDefault",false);
+  }
+  
+  if(GM_getValue("kk_harvestCommentsDefault") == true){
+    var _kk_harvest_option =  document.getElementById("kk_harvest_option");
+    _kk_harvest_option.checked = true;
+    window.harvestComments();
+  }
+  
+  if(!GM_getValue("kk_hideCommentsDefault")){
+    GM_setValue("kk_hideCommentsDefault",false);
+  }
+  
+  if(GM_getValue("kk_hideCommentsDefault") == true){
+    var _kk_hide_option =  document.getElementById("kk_hide_option");
+    _kk_hide_option.checked = true;
+    window.hideComments();
+  }
+  
 };
 
 window.handleClick = function(e){
   var target = e ? e.target : this;
+  console.log(target);
+  
+  if(target.id == "kk_harvest_option"){
+    window.updateOptions(target);
+  }
+  
+  if(target.id == "kk_hide_option"){
+    window.updateOptions(target);
+  }
+  
+  else if(target.id == "kk_hidecomments"){
+    window.hideComments();
+  }
+  else if(target.id == "kk_showcomments"){
+    window.showComments();
+  }
+  
+  //e.stopPropagation();
+  //e.preventDefault();
+  
+};
+
+window.updateOptions = function(target){
+  
+  var _kk_harvest_option =  document.getElementById("kk_harvest_option");
+  var _kk_hide_option =  document.getElementById("kk_hide_option");
+  
+  if(target.id == "kk_harvest_option"){
+    
+    if(_kk_harvest_option.checked == true){
+      GM_setValue("kk_harvestCommentsDefault", true);
+    } else {
+      GM_setValue("kk_harvestCommentsDefault", false);
+    }
+    
+  } else if(target.id == "kk_hide_option"){
+
+    if(_kk_hide_option.checked == true){
+      GM_setValue("kk_hideCommentsDefault", true);
+    } else {
+      GM_setValue("kk_hideCommentsDefault", false);
+    }
+  
+  }
+
 };
 
 window.handleKey = function(e){
   
-  var panel = kommentKillerPanel.firstChild;
+  var panel = kk_contentWrapper.firstChild;
   
   if(e.keyCode == 27){
     panel.style.display == 'block' ? panel.style.display  = 'none' : panel.style.display  = 'block';
@@ -48,31 +131,40 @@ window.handleKey = function(e){
 };
 
 window.harvestComments = function(){
+  
+  // unpack kk_knownBlocks
+  kk_knownBlocks = GM_getValue("kk_blockCache").split(" ");
+  console.log(kk_knownBlocks)
+  
   // check for ids
-  for (i=0; i<knownCommentIdentifiers.length;i++){
-    var _tmpvar = document.getElementById(knownCommentIdentifiers[i]);
+  for (i=0; i<kk_knownBlocks.length;i++){
+    var _tmpvar = document.getElementById(kk_knownBlocks[i]);
     if(_tmpvar){
-      commentBlocks.push(_tmpvar);
+      kk_currentBlocks.push(_tmpvar);
     }
   }
 
   // check for classes
-  for (i=0; i<knownCommentIdentifiers.length;i++){
-    var _tmpvar2 = document.getElementsByClassName(knownCommentIdentifiers[i]);
+  for (i=0; i<kk_knownBlocks.length;i++){
+    var _tmpvar2 = document.getElementsByClassName(kk_knownBlocks[i]);
     if(_tmpvar2 && _tmpvar2.length>1){
       for(j=0;j<_tmpvar2.length;j++){
-        commentBlocks.push(_tmpvar2[j]);
+        kk_currentBlocks.push(_tmpvar2[j]);
       }
     } 
   }
+  
+  console.log(kk_currentBlocks)
+  
+  
 };
 
 window.hideComments = function(){
 
-  if(commentBlocks.length>0){
-    for(i=0;i<commentBlocks.length;i++){
-      if(commentBlocks[i].childNodes.length>1){
-        window.hideElement(commentBlocks[i]);
+  if(kk_currentBlocks.length>0){
+    for(i=0;i<kk_currentBlocks.length;i++){
+      if(kk_currentBlocks[i].childNodes.length>1){
+        window.hideElement(kk_currentBlocks[i]);
       }
     }
   }
@@ -81,10 +173,10 @@ window.hideComments = function(){
 
 window.showComments = function(){
 
-  if(commentBlocks.length>0){
-    for(i=0;i<commentBlocks.length;i++){
-      if(commentBlocks[i].childNodes.length>1){
-        window.showElement(commentBlocks[i]);
+  if(kk_currentBlocks.length>0){
+    for(i=0;i<kk_currentBlocks.length;i++){
+      if(kk_currentBlocks[i].childNodes.length>1){
+        window.showElement(kk_currentBlocks[i]);
       }
     }
   }
@@ -100,5 +192,5 @@ window.hideElement = function(element){
 }
 
 window.addEventListener('load', window.pageLoaded, true);
-window.addEventListener('click', window.handleClick, true);
+kk_contentWrapper.addEventListener('click', window.handleClick, true);
 window.addEventListener('keypress', window.handleKey, true);
