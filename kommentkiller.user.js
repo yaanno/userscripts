5c8ad889b93af4b41b5ca42e1b83a2691bf605ba
yaanno <yaanno@gmail.com>
2009-05-14 15:18:40 +0200

    separate functions, events, event handling

--- kommentkiller.user.js
+++ kommentkiller.user.js
@@ -23,38 +23,82 @@
 // @include       *
 // ==/UserScript==
 
-window.setTimeout(function() {
-
 // TODO externalize the identitifers
 var knownCommentIdentifiers = ['comment','commentlist', 'comments', 'respond', 'cnnComments','comment_container','hozzaszolas'],
 commentBlocks = [];
 
-// check for ids
-for (i=0; i<knownCommentIdentifiers.length;i++){
-  var _tmpvar = document.getElementById(knownCommentIdentifiers[i]);
-  if(_tmpvar){
-    commentBlocks.push(_tmpvar);
+window.pageLoaded = function(){
+  kommentKillerPanel = document.createElement("div");
+  kommentKillerPanel.innerHTML = '<div id="kommentkillerpanel"><style> #kommentkillerpanel {display:none;width:200px;height:80px;background:#000;color:#fff; position:absolute;z-index:1000}</style><p>This will be the panel</p></div>';
+  document.body.insertBefore(kommentKillerPanel, document.body.firstChild);
+};
+
+window.handleClick = function(e){
+  var target = e ? e.target : this;
+};
+
+window.handleKey = function(e){
+  
+  var panel = kommentKillerPanel.firstChild;
+  
+  if(e.keyCode == 27){
+    panel.style.display == 'block' ? panel.style.display  = 'none' : panel.style.display  = 'block';
   }
-}
 
-// check for classes
-for (i=0; i<knownCommentIdentifiers.length;i++){
-  var _tmpvar2 = document.getElementsByClassName(knownCommentIdentifiers[i]);
-  if(_tmpvar2 && _tmpvar2.length>1){
-    for(j=0;j<_tmpvar2.length;j++){
-      commentBlocks.push(_tmpvar2[j]);
-    }
-  } 
-}
+};
 
-// TODO create a hide/show panel with options
-// hide 'em all
-if(commentBlocks.length>0){
-  for(i=0;i<commentBlocks.length;i++){
-    if(commentBlocks[i].childNodes.length>1){
-      commentBlocks[i].style.display = 'none';
+window.harvestComments = function(){
+  // check for ids
+  for (i=0; i<knownCommentIdentifiers.length;i++){
+    var _tmpvar = document.getElementById(knownCommentIdentifiers[i]);
+    if(_tmpvar){
+      commentBlocks.push(_tmpvar);
     }
   }
+
+  // check for classes
+  for (i=0; i<knownCommentIdentifiers.length;i++){
+    var _tmpvar2 = document.getElementsByClassName(knownCommentIdentifiers[i]);
+    if(_tmpvar2 && _tmpvar2.length>1){
+      for(j=0;j<_tmpvar2.length;j++){
+        commentBlocks.push(_tmpvar2[j]);
+      }
+    } 
+  }
+};
+
+window.hideComments = function(){
+
+  if(commentBlocks.length>0){
+    for(i=0;i<commentBlocks.length;i++){
+      if(commentBlocks[i].childNodes.length>1){
+        window.hideElement(commentBlocks[i]);
+      }
+    }
+  }
+
+};
+
+window.showComments = function(){
+
+  if(commentBlocks.length>0){
+    for(i=0;i<commentBlocks.length;i++){
+      if(commentBlocks[i].childNodes.length>1){
+        window.showElement(commentBlocks[i]);
+      }
+    }
+  }
+
+};
+
+window.showElement = function(element){
+  element.style.display = 'block';
 }
 
-}, 0);
+window.hideElement = function(element){
+  element.style.display = 'none';
+}
+
+window.addEventListener('load', window.pageLoaded, true);
+window.addEventListener('click', window.handleClick, true);
+window.addEventListener('keypress', window.handleKey, true);
