8e343855c87e5d3cbbac5037ca10a79129fe8be2
yaanno <yaanno@gmail.com>
2009-05-17 19:12:34 +0200

    console, extra config options, enhancements added

--- kommentkiller.user.js
+++ kommentkiller.user.js
@@ -1,6 +1,6 @@
 // KommentKiller user script
-// version 1.0 pre-pre-pre-dirty-alpha
-// 2009-05-12
+// version 1.0 alpha
+// 2009-05-17
 // Released under the Public Domain
 //
 // --------------------------------------------------------------------
@@ -29,13 +29,14 @@ var $KK = window.KK || {};
 $KK.knownBlocks = "comment commentlist comments respond cnnComments comment_container trackbacks";
 $KK.currentBlocks = [];
 $KK.contentWrapper = document.createElement("div");
+$KK.console = null;
 $KK.panelConfig = {
-  internalBlockCache : [],
   harvest_option : false,
   hide_option : false,
   hide_input : null,
   harvest_input : null,
-  panel : null
+  panel : null,
+  known_textarea : null
 };
 
 // the init function
@@ -43,16 +44,25 @@ $KK.pageLoaded = function(){
 
   // create control panel
   $KK.createPanel();
+  
+  // debug
+  $KK.console = $KK.byId("kk_console");
+  var _panel = $KK.byId("kk_panel");
+  if(_panel){
+    $KK.panelConfig.panel = _panel;
+    $KK.log("Panel created")
+  } else {
+    $KK.log("Panel not created");
+  }
+
 
   // setup default options for persistent storage
   if(!GM_getValue("kk_blockCache")){
     GM_setValue("kk_blockCache",$KK.knownBlocks);
   }
-
   if(!GM_getValue("kk_hideCommentsDefault")){
     GM_setValue("kk_hideCommentsDefault",false);
   }
-
   if(!GM_getValue("kk_harvestCommentsDefault")){
     GM_setValue("kk_harvestCommentsDefault",false);
   }
@@ -63,15 +73,16 @@ $KK.pageLoaded = function(){
 
   $KK.panelConfig.harvest_input = $KK.byId("kk_harvest_checkbox");
   $KK.panelConfig.hide_input = $KK.byId("kk_hide_checkbox");
-
-  $KK.setPanelState();
-
-};
-
-$KK.setPanelState = function(){
-
+  $KK.panelConfig.known_texarea = $KK.byId("kk_knownblocks");
+  
+  
+  // set default panel state
   $KK.panelConfig.hide_input.checked = $KK.panelConfig.hide_option;
   $KK.panelConfig.harvest_input.checked = $KK.panelConfig.harvest_option;
+  $KK.panelConfig.known_texarea.value = GM_getValue("kk_blockCache");
+  
+  // do the default processing
+  $KK.doProcess();
   
 };
 
@@ -80,28 +91,42 @@ $KK.createPanel = function(){
 
   var kk_content = '<div id="kk_panel">';
   kk_content += '<style>';
-  kk_content += '#kk_panel {display:none;width:200px;padding:15px;background:#333;color:#fff; position:fixed;top:0;left:20px;z-index:1000;font-size:11px; font-family:Helvetica;border: 5px solid #666;border-top:none}';
-  kk_content += '#kk_panel *{margin:0;padding:0;text-align:left} #kk_panel p{padding: 5px; line-height: 16px}';
-  kk_content += '#kk_panel a:link, #kk_panel a:visited{color:#fff} #kk_panel a:hover{color:#ddd}';
+  kk_content += '#kk_panel {display:none;min-width:200px;padding:15px;background:#333;position:fixed;top:0;left:20px;z-index:1000;font-size:11px; font-family:Helvetica;border: 5px solid #666;border-top:none}';
+  kk_content += '#kk_panel *{margin:0;padding:0;text-align:left;color:#fff;font-size:11px} #kk_panel p{padding: 5px; line-height: 16px}';
+  kk_content += '#kk_panel * input{background: #fefefe; color: #000; font-weight: bold}';
+  kk_content += '#kk_console {border: 1px solid #fff; background: #9f3; color: #333}';
   kk_content += '</style>';
-  kk_content += '<p><input id="kk_harvest_checkbox" type="checkbox"> <label for="kk_harvest_option">Harvest all comments by default</label></p>';
-  kk_content += '<p><input id="kk_hide_checkbox" type="checkbox"> <label for="kk_hide_option">Hide all comments by default</label></p>';
+  kk_content += '<p><input id="kk_harvest_checkbox" type="checkbox" /> <label for="kk_harvest_checkbox">Harvest all comments by default</label></p>';
+  kk_content += '<p><input id="kk_hide_checkbox" type="checkbox" /> <label for="kk_hide_checkbox">Hide all comments by default</label></p>';
   kk_content += '<p><input id="kk_hidecomments" type="button" value="Hide all"> or ';
   kk_content += '<input id="kk_showcomments" type="button" value="Show all"></p>';
+  kk_content += '<p>Known identifiers (separate values with space) <br>';
+  kk_content += '<input type="text" id="kk_knownblocks" /> <input id="kk_knownblocks_save" type="button" value="Save"></p>';
+  kk_content += '<p id="kk_console">This is the console</p>';
   kk_content += '</div>';
 
   $KK.contentWrapper.innerHTML = kk_content;
   document.body.insertBefore($KK.contentWrapper, document.body.firstChild);
 
-  var _panel = $KK.byId("kk_panel");
-  if(_panel){
-    $KK.panelConfig.panel = _panel;
-  } else {
-    console.log("panel not creaed");
-  }
+};
 
-  console.log($KK.panelConfig);
+$KK.log = function(msg){
 
+  $KK.console.innerHTML = msg;
+
+};
+
+
+$KK.doProcess = function(){
+
+  if($KK.panelConfig.harvest_option){
+    $KK.getComments();
+  };
+  
+  if($KK.panelConfig.hide_option){
+    $KK.hideComments();
+  };
+  
 };
 
 // Handles control panel events
@@ -109,7 +134,6 @@ $KK.createPanel = function(){
 $KK.handleClick = function(e){
 
   var target = e ? e.target : this;
-  //console.log(target);
 
   if( (target.id == "kk_harvest_checkbox") || (target.id == "kk_hide_checkbox") ){
     $KK.updateOptions(target);
@@ -122,33 +146,36 @@ $KK.handleClick = function(e){
   else if(target.id == "kk_showcomments"){
     $KK.showComments();
   }
+  
+  else if(target.id == "kk_knownblocks_save"){
+    $KK.updateKnownBlocks();
+  }
 
 };
 
 // Updates the persistent storage options
-// TODO clean up this function using encapsulation
 $KK.updateOptions = function(target){
 
-  var value = false;
-  var checkbox = null;
-
+  var checkbox = $KK.byId(target.id);
+  var value = true ? checkbox.checked  : false;
+  
   if(target.id == "kk_harvest_checkbox"){
-    var checkbox = $KK.panelConfig.harvest_option;
-    value = true ? checkbox  : false;
-    console.log("harvest set to " + value)
     GM_setValue("kk_harvestCommentsDefault", value);
-
   } else if(target.id == "kk_hide_checkbox"){
-    var checkbox = $KK.panelConfig.hide_option;
-    console.log(checkbox)
-    value = true ? checkbox  : false;
-    console.log("hide set to " + value)
     GM_setValue("kk_hideCommentsDefault", value);
-
   }
 
-  //return;
+};
 
+$KK.updateKnownBlocks = function(){
+  
+  var input = $KK.panelConfig.known_texarea;
+  
+  if( (input.value != "") && ($KK.knownBlocks != input.value) ){
+    $KK.knownBlocks = input.value;
+    GM_setValue("kk_blockCache",$KK.knownBlocks);
+  }
+  
 };
 
 // Handles keyboard events watching the open/close key press (currently: ESC)
@@ -168,7 +195,6 @@ $KK.getComments = function(){
 
   // unpack kk_knownBlocks
   $KK.knownBlocks = GM_getValue("kk_blockCache").split(" ");
-  console.log($KK.knownBlocks);
 
   // check for ids
   for (i=0; i<$KK.knownBlocks.length;i++){
@@ -187,9 +213,8 @@ $KK.getComments = function(){
       }
     }
   }
-
-  console.log($KK.currentBlocks);
-
+  
+  $KK.log("Got " + $KK.currentBlocks.length + " comment blocks");
 
 };
 
@@ -197,13 +222,16 @@ $KK.getComments = function(){
 $KK.hideComments = function(){
 
   if($KK.currentBlocks.length>0){
-    for(i=0, _len=kk_currentBlocks.length; i<_len;i++){
+    for(i=0, _len=$KK.currentBlocks.length; i<_len;i++){
       if($KK.currentBlocks[i].childNodes.length>1){
         $KK.hide($KK.currentBlocks[i]);
       }
     }
+    
+    $KK.log($KK.currentBlocks.length + " elements hidden");
+    
   } else {
-    console.log("nothing to hide");
+    $KK.log("nothing to hide");
   }
 
 };
@@ -212,13 +240,16 @@ $KK.hideComments = function(){
 $KK.showComments = function(){
 
   if($KK.currentBlocks.length>0){
-    for(i=0, _len=kk_currentBlocks.length;i<_len;i++){
+    for(i=0, _len=$KK.currentBlocks.length;i<_len;i++){
       if($KK.currentBlocks[i].childNodes.length>1){
         $KK.show($KK.currentBlocks[i]);
       }
     }
+    
+    $KK.log($KK.currentBlocks.length + " elements got visible");
+    
   } else {
-    console.log("nothing to show");
+    $KK.log("nothing to show");
   }
 
 };
